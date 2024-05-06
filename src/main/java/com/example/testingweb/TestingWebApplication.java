bbad315614a96d8d44c26ad3f89d36b03f580d97
RafaBarreraRav <167454023+RafaBarreraRav@users.noreply.github.com>
2024-05-06 12:23:37 -0400

    Update TestingWebApplication.java

--- src/main/java/com/example/testingweb/TestingWebApplication.java
+++ src/main/java/com/example/testingweb/TestingWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestingWebApplication {
+	String password = "1234";
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingWebApplication.class, args);
